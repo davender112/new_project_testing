a6f14ad4dc05edc918b7655685db5de90598975b
Davender Singh <singh.davender.11@gmail.com>
2012-02-12 09:26:53 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FundooT::Application.config.secret_token = '6937ee17ca8561b38c8647715edd6d875bf98711f8e590baca2e1a7097f388744f9f3cb217b075d4691fe852ced0f17ea0f8779ebe3394fee1479b7271e8dbf6'
